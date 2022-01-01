8dd7cb257890881a9cb6336b95e0269e5565e575
cebasypia.dev01 <cebasypia.tskm8+dev01@gmail.com>
2022-01-01 19:35:16 +0900

    Move DB_URL to env.py

--- migration/env.py
+++ migration/env.py
@@ -9,6 +9,13 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+# this will overwrite the ini-file sqlalchemy.url path
+# with the path given in the config of the main code
+config.set_main_option(
+    "sqlalchemy.url",
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/alembic_db",
+)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
