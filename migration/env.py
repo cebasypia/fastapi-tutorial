3add15374440403348eabcfe3fdaa9772a702b71
cebasypia.dev01 <cebasypia.tskm8+dev01@gmail.com>
2022-01-01 20:02:15 +0900

    Move DB_URL to env.py

--- migration/env.py
+++ migration/env.py
@@ -4,6 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from config import DB_HOST, DB_PASSWORD, DB_PORT, DB_USER
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
